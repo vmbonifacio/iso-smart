2b929cd7e2a0d152893b6435364ee52bff809743
Qu3qu3j4n418 <manuel.vbq18@gmail.com>
2023-04-19 11:07:28 -0500

    nueva-actualizacion

---
+++ src/main/java/controlador/Controlador.java
@@ -0,0 +1,86 @@
+package controlador;
+
+import modeloDAO.PersonaDAO;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.Persona;
+
+public class Controlador extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Controlador</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Controlador at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Obtén los datos ingresados por el usuario desde los parámetros de la solicitud
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        int edad = Integer.parseInt(request.getParameter("edad"));
+        String correo = request.getParameter("correo");
+
+        Persona persona = new Persona(0, nombre, apellido, edad, correo); // El ID se inicializa en 0 ya que se generará automáticamente en la base de datos
+
+        PersonaDAO personaDAO = new PersonaDAO();
+        personaDAO.guardarPersona(persona);
+
+        // Envía un mensaje de confirmación al usuario
+        PrintWriter out = response.getWriter();
+        out.println("<html><body><h2>Persona guardada exitosamente</h2></body></html>");
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
